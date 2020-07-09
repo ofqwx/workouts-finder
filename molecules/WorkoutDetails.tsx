62e727416d6d10d927c8afa92d67c9cb33133678
Antonio Aznarez <antonio.aznarez@elinvar.de>
2020-07-09 15:05:45 +0200

    Fix loading state in details

--- molecules/WorkoutDetails.tsx
+++ molecules/WorkoutDetails.tsx
@@ -9,32 +9,61 @@ type TWorkoutDetailsProps = {
 };
 
 export default function WorkoutDetails({ id }: TWorkoutDetailsProps) {
-  const [workoutDetails, setWorkoutDetails] = useState<TWorkout | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [workoutDetails, setWorkoutDetails] = useState<
+    TWorkout | Record<string, unknown>
+  >({});
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     async function getWorkoutDetails() {
       try {
-        setLoading(true);
-
         const response = await fetch(`/api/workout/detail/${id}`);
         const data = await response.json();
 
         setWorkoutDetails(data);
-        setLoading(false);
       } catch (error) {
-        throw new Error(error);
+        setError(true);
+        console.error(error);
       }
     }
 
     getWorkoutDetails();
   }, [id]);
 
-  if (loading || !workoutDetails) {
+  if (Object.keys(workoutDetails).length) {
     return (
       <Flex direction="column">
         <Box alignSelf="center">
-          <Heading level={2}>loading...</Heading>
+          <Heading level={1} margin="0">
+            {workoutDetails.name}{" "}
+            <small>{`cat: ${workoutDetails.category}`}</small>
+          </Heading>
+        </Box>
+
+        <Flex justifyContent="center">
+          <Box flexBasis="800px">
+            <Flex direction="column">
+              <Box alignSelf="start">
+                <Heading level={3} margin="0">{`Start date: ${formatDate(
+                  workoutDetails.startDate
+                )}`}</Heading>
+              </Box>
+
+              <Box alignSelf="start">
+                <p>{workoutDetails.description}</p>
+              </Box>
+            </Flex>
+          </Box>
+        </Flex>
+      </Flex>
+    );
+  }
+
+  if (error) {
+    return (
+      <Flex direction="column">
+        <Box alignSelf="center">
+          <p>Something went wrong, please contact support.</p>
         </Box>
       </Flex>
     );
@@ -43,27 +72,8 @@ export default function WorkoutDetails({ id }: TWorkoutDetailsProps) {
   return (
     <Flex direction="column">
       <Box alignSelf="center">
-        <Heading level={1} margin="0">
-          {workoutDetails.name}{" "}
-          <small>{`cat: ${workoutDetails.category}`}</small>
-        </Heading>
+        <Heading level={2}>Loading...</Heading>
       </Box>
-
-      <Flex justifyContent="center">
-        <Box flexBasis="800px">
-          <Flex direction="column">
-            <Box alignSelf="start">
-              <Heading level={3} margin="0">{`Start date: ${formatDate(
-                workoutDetails.startDate
-              )}`}</Heading>
-            </Box>
-
-            <Box alignSelf="start">
-              <p>{workoutDetails.description}</p>
-            </Box>
-          </Flex>
-        </Box>
-      </Flex>
     </Flex>
   );
 }
